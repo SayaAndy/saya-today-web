97e30570056309bda28fc41acba27c01fa1ef9f3
SayaAndy <montferrat@tuta.io>
2025-07-26 22:01:30 +0700

    refactor: use tailwindcss tool for generating css (vpn required no more for russia) refactor: move css to sayana-static bucket refactor: first touch of rendering templates

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-	app := fiber.New()
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Демо-материал после демон-аэропорта",
+		})
+	})
 
 	app.Static("/", "./public")
 
