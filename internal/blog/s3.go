1c441f5eab9a2a24fe5309178ce010f32adac1c7
SayaAndy <saya.andy@posteo.com>
2026-05-17 12:19:56 +0700

    feat: take all metadata from centralized index.json

--- internal/blog/s3.go
+++ internal/blog/s3.go
@@ -1,6 +1,7 @@
 package blog
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -86,34 +87,72 @@ func (c *S3Client) Scan(prefix string) ([]*Page, error) {
 
 	fullPrefix := c.prefix + prefix
 	pages := make([]*Page, 0)
-	for catKey, cat := range idx.Categories {
-		lang, ok := strings.CutPrefix(catKey, c.prefix)
-		if !ok {
-			continue
-		}
-		if wantLang != "" && wantLang != lang {
-			continue
-		}
-		for _, e := range cat.Pages {
-			if !strings.HasPrefix(e.Link, fullPrefix) {
+
+	switch idx.SchemaVersion {
+	case 1:
+		for catKey, cat := range *idx.Categories.(*map[string]*IndexV1Category) {
+			lang, ok := strings.CutPrefix(catKey, c.prefix)
+			if !ok {
 				continue
 			}
-			fileName := e.Link[strings.LastIndex(e.Link, "/")+1 : strings.LastIndex(e.Link, ".")]
-			pages = append(pages, &Page{
-				Link:         e.Link,
-				FileName:     fileName,
-				Lang:         lang,
-				ModifiedTime: e.ModifiedTime,
-				Metadata: &frontmatter.Metadata{
-					Title:            e.Title,
-					ShortDescription: e.ShortDescription,
-					ActionDate:       e.ActionDate,
-					PublishedTime:    e.PublishedTime,
-					Thumbnail:        e.Thumbnail,
-					Tags:             e.Tags,
-					Geolocation:      e.Geolocation,
-				},
-			})
+			if wantLang != "" && wantLang != lang {
+				continue
+			}
+			for _, e := range cat.Pages {
+				if !strings.HasPrefix(e.Link, fullPrefix) {
+					continue
+				}
+				fileName := e.Link[strings.LastIndex(e.Link, "/")+1 : strings.LastIndex(e.Link, ".")]
+				pages = append(pages, &Page{
+					Link:         e.Link,
+					FileName:     fileName,
+					Lang:         lang,
+					ModifiedTime: e.ModifiedTime,
+					Metadata: &frontmatter.Metadata{
+						Title:            e.Title,
+						ShortDescription: e.ShortDescription,
+						ActionDate:       e.ActionDate,
+						PublishedTime:    e.PublishedTime,
+						Thumbnail:        e.Thumbnail,
+						Tags:             e.Tags,
+						Geolocation:      e.Geolocation,
+						Medley:           e.Medley,
+						MedleyPart:       e.MedleyPart,
+					},
+				})
+			}
+		}
+	case 2:
+		for catKey, cat := range *idx.Categories.(*map[string]*IndexV2Category) {
+			lang, ok := strings.CutPrefix(catKey, c.prefix)
+			if !ok {
+				continue
+			}
+			if wantLang != "" && wantLang != lang {
+				continue
+			}
+			for codename, e := range cat.Pages {
+				if !strings.HasPrefix(e.Link, fullPrefix) {
+					continue
+				}
+				pages = append(pages, &Page{
+					Link:         e.Link,
+					FileName:     codename,
+					Lang:         lang,
+					ModifiedTime: e.ModifiedTime,
+					Metadata: &frontmatter.Metadata{
+						Title:            e.Title,
+						ShortDescription: e.ShortDescription,
+						ActionDate:       e.ActionDate,
+						PublishedTime:    e.PublishedTime,
+						Thumbnail:        e.Thumbnail,
+						Tags:             e.Tags,
+						Geolocation:      e.Geolocation,
+						Medley:           e.Medley,
+						MedleyPart:       e.MedleyPart,
+					},
+				})
+			}
 		}
 	}
 
@@ -121,9 +160,13 @@ func (c *S3Client) Scan(prefix string) ([]*Page, error) {
 }
 
 func (c *S3Client) ReadAll(path string) ([]byte, error) {
+	return c.readAll(c.prefix + path)
+}
+
+func (c *S3Client) readAll(path string) ([]byte, error) {
 	output, err := c.s3cl.GetObject(context.Background(), &s3.GetObjectInput{
 		Bucket: aws.String(c.bucketName),
-		Key:    aws.String(c.prefix + path),
+		Key:    aws.String(path),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("get S3 object: %w", err)
@@ -139,9 +182,39 @@ func (c *S3Client) ReadAll(path string) ([]byte, error) {
 }
 
 func (c *S3Client) ReadFrontmatter(path string) (metadata *frontmatter.Metadata, markdown []byte, err error) {
+	idxRaw, err := c.readAll(IndexFileName)
+	if err != nil {
+		return nil, nil, fmt.Errorf("read %s: %w", IndexFileName, err)
+	}
+
+	var idx Index
+	if err := json.Unmarshal(idxRaw, &idx); err != nil {
+		return nil, nil, fmt.Errorf("unmarshal %s: %w", IndexFileName, err)
+	}
+
 	contentBytes, err := c.ReadAll(path)
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to read file for frontmatter parsing: %w", err)
+	}
+
+	switch idx.SchemaVersion {
+	case 1:
+		return frontmatter.ParseFrontmatter(contentBytes)
+	case 2:
+		fullPath := c.prefix + path
+		page := (*idx.Categories.(*map[string]*IndexV2Category))[fullPath[:strings.LastIndex(fullPath, "/")]].Pages[fullPath[strings.LastIndex(fullPath, "/")+1:strings.LastIndex(fullPath, ".")]]
+		metadata = page.Metadata()
+
+		if !bytes.HasPrefix(contentBytes, []byte("---\n")) {
+			return metadata, contentBytes, nil
+		}
+
+		end := bytes.Index(contentBytes[4:], []byte("\n---\n"))
+		if end == -1 {
+			return metadata, contentBytes, nil
+		}
+
+		return metadata, contentBytes[end+9:], nil
 	}
 
 	return frontmatter.ParseFrontmatter(contentBytes)
